5667a70d59527d7b6ab9ed76972e1ecf989c154c
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-02 04:29:54 +0530

    fix: nav buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -34,26 +34,26 @@ const Navbar = () => {
       <OrganizationToggler />
 
       {/* Navigation Buttons */}
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-2.5">
         {userNavigations.map((navigation, index) => (
           <div key={index}>
             <Button
               variant={"outline"}
               size={"icon"}
-              className="rounded-xl border-zinc-300"
+              className="rounded-xl border-zinc-400/60 shadow-none p-0"
             >
-              <navigation.icon className="w-4 h-4" />
+              <navigation.icon className="w-5 h-5 text-zinc-700" />
             </Button>
           </div>
         ))}
 
-        <div className="rounded-full inline-flex items-center justify-center w-8 h-8 cursor-pointer bg-violet-600">
+        <div className="rounded-full inline-flex items-center justify-center w-9 h-9 cursor-pointer bg-violet-600">
           <Image
             src="/animoji.jpg"
             alt="User"
-            width={32}
-            height={32}
-            className="w-full h-full rounded-full"
+            width={36}
+            height={36}
+            className="w-full h-full rounded-full border border-zinc-400/60"
           />
         </div>
       </div>
