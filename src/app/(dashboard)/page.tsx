6679c7d751d934356060b785e960917dad4619a8
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-02 03:29:25 +0530

    feat: Dashboard header

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { CalendarRange, ChevronDown, SlidersHorizontal } from "lucide-react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-6">
+      {/* Dashboard Header */}
+      <div className="flex items-center justify-between w-full">
+        <h1 className="font-semibold text-zinc-900 text-2xl">
+          Welcome, Tanay!
+        </h1>
+
+        <div className="flex items-center gap-2">
+          <Button variant={"outline"} className="rounded-lg">
+            <SlidersHorizontal className="h-4 w-4" />
+            Customize home view
+          </Button>
+
+          <Button variant={"outline"} className="rounded-lg">
+            <CalendarRange className="h-4 w-4" />
+            This week
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
