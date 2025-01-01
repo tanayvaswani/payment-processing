d67dd8fb4752a681747df607e0830719078e689b
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-02 02:32:49 +0530

    feat: Nav Buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,30 @@
-import { ShieldQuestionIcon } from "lucide-react";
+import { BellDot, CircleHelp, CodeXml, Settings } from "lucide-react";
 
 import { Button } from "./ui/button";
 
 import OrganizationToggler from "./org-toggler";
+import Image from "next/image";
 
 const userNavigations = [
   {
     title: "Support",
     link: "/support",
-    icon: ShieldQuestionIcon,
+    icon: CircleHelp,
+  },
+  {
+    title: "Framework",
+    link: "/framework",
+    icon: CodeXml,
+  },
+  {
+    title: "Notifications",
+    link: "/notifications",
+    icon: BellDot,
+  },
+  {
+    title: "Settings",
+    link: "/settings",
+    icon: Settings,
   },
 ];
 
@@ -21,11 +37,25 @@ const Navbar = () => {
       <div className="flex items-center gap-3">
         {userNavigations.map((navigation, index) => (
           <div key={index}>
-            <Button variant={"outline"} size={"icon"}>
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              className="rounded-xl border-zinc-300"
+            >
               <navigation.icon className="w-4 h-4" />
             </Button>
           </div>
         ))}
+
+        <div className="rounded-full inline-flex items-center justify-center w-8 h-8 cursor-pointer bg-violet-600">
+          <Image
+            src="/animoji.jpg"
+            alt="User"
+            width={32}
+            height={32}
+            className="w-full h-full rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
