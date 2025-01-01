212a771169104fa8c8c94132c754ccf3ecf356b6
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-02 03:29:42 +0530

    chore: imports

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { CalendarRange, ChevronDown, SlidersHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
   return (
