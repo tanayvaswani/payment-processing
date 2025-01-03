8ec3511e9b58ef450e5a305707b57d91c19d89d9
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-03 19:24:21 +0530

    feat: profile dropdown

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,21 @@
-import { BellDot, CircleHelp, CodeXml, Settings } from "lucide-react";
+import {
+  BellDot,
+  ChevronDown,
+  CircleHelp,
+  CodeXml,
+  Settings,
+} from "lucide-react";
 import Image from "next/image";
 
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import OrganizationToggler from "./org-toggler";
 
@@ -34,7 +48,7 @@ const Navbar = () => {
       <OrganizationToggler />
 
       {/* Navigation Buttons */}
-      <div className="flex items-center gap-2.5">
+      <div className="hidden md:flex items-center gap-2.5">
         {userNavigations.map((navigation, index) => (
           <div key={index}>
             <Button
@@ -56,6 +70,44 @@ const Navbar = () => {
             className="w-full h-full rounded-full border border-zinc-400/60"
           />
         </div>
+      </div>
+
+      <div className="flex md:hidden items-center gap-2.5">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex items-center">
+            <div className="rounded-full inline-flex items-center justify-center w-9 h-9 cursor-pointer bg-violet-600">
+              <Image
+                src="/animoji.jpg"
+                alt="User"
+                width={36}
+                height={36}
+                className="w-full h-full rounded-full border border-zinc-400/60"
+              />
+            </div>
+
+            <ChevronDown className="w-4 h-4 text-zinc-700" />
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="max-w-64 mx-2">
+            <DropdownMenuLabel className="flex min-w-0 flex-col">
+              <span className="truncate text-sm font-medium text-foreground">
+                Tanay
+              </span>
+              <span className="truncate text-xs font-normal text-muted-foreground">
+                tanayvaswani.work@gmail.com
+              </span>
+            </DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+
+            {userNavigations.map((navigation, index) => (
+              <DropdownMenuItem key={index}>
+                <navigation.icon className="w-5 h-5 text-zinc-700 mr-1" />
+                <span>{navigation.title}</span>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
